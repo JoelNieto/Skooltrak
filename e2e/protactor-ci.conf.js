a15387d0d8574e297b8cba527d3abf41dc58c5f8
Joel Nieto <joelnietotec@outlook.com>
2020-01-17 06:16:38 -0500

    ci

---
+++ e2e/protactor-ci.conf.js
@@ -0,0 +1,8 @@
+const config = require('./protractor.conf').config;
+const puppeteer = require('puppeteer');config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: ['--headless', '--no-sandbox'],
+    binary: puppeteer.executablePath()
+  }
+};exports.config = config;
