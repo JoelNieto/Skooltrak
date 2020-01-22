29b71a8e35755cbca0d2c69d034025964c8c708a
Joel Nieto <joelnietotec@outlook.com>
2020-01-22 06:08:26 -0500

    changes

--- src/app/admin/collection/collection.module.ts
+++ src/app/admin/collection/collection.module.ts
@@ -1,16 +1,17 @@
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
-import { CalendarModule } from 'angular-calendar';
 import { CustomComponentsModule } from '@skooltrak/custom-components';
+import { CalendarModule } from 'angular-calendar';
 
 import { CollectionComponent } from './collection.component';
 import { CollectionRoutingModule } from './collections.routes';
 import { EnrollCostsComponent } from './enroll-costs/enroll-costs.component';
 import { PaymentDaysComponent } from './payment-days/payment-days.component';
 import { PaymentsComponent } from './payments/payments.component';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { PaymentsComponent } from './payments/payments.component';
     NgbTabsetModule,
     CollectionRoutingModule,
     CalendarModule,
+    NgbModule,
     FormsModule,
     CustomComponentsModule,
     TranslateModule.forChild()
--- src/app/admin/collection/payments/payments.component.spec.ts
+++ src/app/admin/collection/payments/payments.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CustomComponentsModule } from '@skooltrak/custom-components';
 
 import { PaymentsComponent } from './payments.component';
+import { DatePipe } from '@angular/common';
 
 describe('PaymentsComponent', () => {
   let component: PaymentsComponent;
@@ -8,7 +11,9 @@ describe('PaymentsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PaymentsComponent ]
+      imports: [ HttpClientTestingModule,  CustomComponentsModule ],
+      declarations: [ PaymentsComponent ],
+      providers: [ DatePipe ]
     })
     .compileComponents();
   }));
