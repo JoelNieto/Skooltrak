662de9dfb7e7dd80e29098bf4bee5919adbb782b
Joel Nieto <joelnietotec@outlook.com>
2020-01-25 10:11:45 -0500

    changes

---
+++ src/app/admin/students/charges-form/charges-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChargesFormComponent } from './charges-form.component';
+
+describe('ChargesFormComponent', () => {
+  let component: ChargesFormComponent;
+  let fixture: ComponentFixture<ChargesFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChargesFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChargesFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
