b90fe6d82f0c5137e63c34fee7db3c4efbb6e88c
Joel Nieto <joelnietotec@outlook.com>
2020-03-18 01:08:46 -0500

    add-reply

--- src/app/teachers/forums/forums.module.ts
+++ src/app/teachers/forums/forums.module.ts
@@ -1,21 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModalModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbModalModule,
+  NgbNavModule,
+  NgbTooltipModule
+} from '@ng-bootstrap/ng-bootstrap';
 import { TranslocoModule } from '@ngneat/transloco';
 import { CustomComponentsModule } from '@skooltrak/custom-components';
 import { NgxSummernoteModule } from 'ngx-summernote';
 import { DocumentsFormModule } from 'src/app/shared/components/documents-form/documents-form.module';
+import { AvatarPipe } from 'src/app/shared/pipes/avatar.pipe';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ChatComponent } from './chat/chat.component';
+import { DocumentsComponent } from './documents/documents.component';
 import { ForumsPageComponent } from './forums-page/forums-page.component';
 import { ForumsRoutingModule } from './forums-routing.module';
 import { ForumsComponent } from './forums.component';
-import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
-  declarations: [ForumsComponent, ForumsPageComponent, ChatComponent, DocumentsComponent],
+  declarations: [
+    ForumsComponent,
+    ForumsPageComponent,
+    ChatComponent,
+    DocumentsComponent
+  ],
   imports: [
     CommonModule,
     ForumsRoutingModule,
@@ -23,11 +33,13 @@ import { DocumentsComponent } from './documents/documents.component';
     SharedModule,
     NgbNavModule,
     NgbModalModule,
+    NgbTooltipModule,
     NgxSummernoteModule,
     DocumentsFormModule,
     CustomComponentsModule,
     ReactiveFormsModule,
     TranslocoModule
-  ]
+  ],
+  providers: [AvatarPipe]
 })
 export class ForumsModule {}
