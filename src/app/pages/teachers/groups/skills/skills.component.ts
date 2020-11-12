87569d13edeb36c60ef9b5749cb500ffd4f12840
Joel Nieto <joelnietotec@outlook.com>
2020-11-12 08:12:24 -0500

    cambios-varios

---
+++ src/app/pages/teachers/groups/skills/skills.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Observable } from 'rxjs';
+import { StorageEnum } from 'src/app/shared/enums/storage.enum';
+import { Period } from 'src/app/shared/models/periods.model';
+import { Skill, StudentSkill } from 'src/app/shared/models/skills.model';
+import { Student } from 'src/app/shared/models/students.model';
+import { SkillsService } from 'src/app/shared/services/skills.service';
+import { StorageService } from 'src/app/shared/services/storage.service';
+import { StudentsService } from 'src/app/shared/services/students.service';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.sass'],
+})
+export class SkillsComponent implements OnInit, OnChanges {
+  @Input() student: Student;
+  skills: Observable<Skill[]>;
+  periods: Observable<Period[]>;
+  items: Observable<StudentSkill[]>;
+  constructor(
+    private skillsService: SkillsService,
+    private studentService: StudentsService,
+    private storage: StorageService
+  ) {}
+
+  ngOnInit(): void {
+    this.periods = this.storage.getFromStorage(StorageEnum.Periods);
+    this.skills = this.skillsService.getAll();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.student) {
+      if (this.student) {
+        this.items = this.studentService.getSkills(this.student.id);
+      }
+    }
+  }
+}
--- src/app/shared/models/skills.model.ts
+++ src/app/shared/models/skills.model.ts
@@ -1,6 +1,18 @@
+import { Period } from './periods.model';
+import { Reference } from './users.model';
+
 export interface Skill {
   id: string;
   name: string;
   description: string;
   active: boolean;
+}
+
+export interface StudentSkill {
+  id: string;
+  student: Reference;
+  skill: Skill;
+  period: Period;
+  year: number;
+  value: string;
 }
