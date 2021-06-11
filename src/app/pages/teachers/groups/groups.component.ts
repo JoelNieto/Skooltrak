5ac6d449bea073209c0aac9f8d4959e74255e00f
JoelNietoTec <joel@skooltrak.com>
2021-06-10 23:17:16 -0500

    table-permissions

--- src/app/pages/teachers/groups/groups.component.ts
+++ src/app/pages/teachers/groups/groups.component.ts
@@ -22,7 +22,6 @@ export class GroupsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.table.permissions.read = false;
     this.table.columns = [
       {
         name: 'name',
