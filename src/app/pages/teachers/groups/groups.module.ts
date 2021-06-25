1411b41c96f7c533136a4756cc200ae26c173bf0
JoelNietoTec <joel@skooltrak.com>
2021-06-25 18:16:20 -0500

    Preupdate

--- src/app/pages/teachers/groups/groups.module.ts
+++ src/app/pages/teachers/groups/groups.module.ts
@@ -6,16 +6,24 @@ import { TranslocoModule } from '@ngneat/transloco';
 import { CustomComponentsModule } from '@skooltrak/custom-components';
 
 import { DetailsComponent } from './details/details.component';
+import { EvaluationComponent } from './evaluation/evaluation.component';
 import { GradesDetailsComponent } from './grades-details/grades-details.component';
 import { GradesPeriodComponent } from './grades-period/grades-period.component';
 import { GradesComponent } from './grades/grades.component';
 import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupsComponent } from './groups.component';
 import { SkillsComponent } from './skills/skills.component';
-import { EvaluationComponent } from './evaluation/evaluation.component';
 
 @NgModule({
-  declarations: [GroupsComponent, DetailsComponent, GradesComponent, GradesDetailsComponent, GradesPeriodComponent, SkillsComponent, EvaluationComponent],
+  declarations: [
+    GroupsComponent,
+    DetailsComponent,
+    GradesComponent,
+    GradesDetailsComponent,
+    GradesPeriodComponent,
+    SkillsComponent,
+    EvaluationComponent,
+  ],
   imports: [
     CommonModule,
     GroupsRoutingModule,
