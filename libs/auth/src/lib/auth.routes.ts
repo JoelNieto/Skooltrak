d7540272aaf72d921c60b1a2d8000f01562cbf18
Joel Nieto <joelnieto1215@gmail.com>
2022-06-29 22:39:22 -0500

    adding state lib

--- libs/auth/src/lib/auth.routes.ts
+++ libs/auth/src/lib/auth.routes.ts
@@ -4,8 +4,13 @@ import { SignInComponent } from './sign-in/sign-in.component';
 
 export const AUTH_ROUTES: Routes = [
   {
-    path: 'sign-in',
-    component: SignInComponent,
+    path: '',
+    children: [
+      {
+        path: 'sign-in',
+        component: SignInComponent,
+      },
+      { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
+    ],
   },
-  { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
 ];
